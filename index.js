6fd0abe63f319ad26954e68f13636e4da32aa2b4
Anas Iqbal <anasss1992@gmail.com>
2019-12-04 22:38:17 +0530

    changes commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); //return function
 const app = express(); // return object of type express
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "Anas" });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
