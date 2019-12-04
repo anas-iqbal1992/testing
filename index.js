ad468b62118901a6c946c033b2b2f0b334b99607
Anas Iqbal <anasss1992@gmail.com>
2019-12-04 21:59:45 +0530

    bug fixed

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express(); // return object of type express
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
-const PORT = process.evn.PORT || 5000;
-app.listen(5000, () => console.log(`listing at port no 5000`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
