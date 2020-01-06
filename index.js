476005e4ddb2584723da08301da2ecc487c391ae
Anas Iqbal <anasss1992@gmail.com>
2020-01-06 15:50:17 +0530

    fineshed auth flow

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express"); //return function
-const app = express(); // return object of type express
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require("./models/User");
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Anas" });
-});
+mongoose.connect(keys.mongoURI);
+const app = express(); // return object of type express
+app.use(
+  cookieSession({
+    maxAge:30 * 24 * 60 * 60 *1000,
+    keys:[keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
